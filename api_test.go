08e00bfb9f67b49b279ee7279e2ae60c3775b093
Anna Cruz <anna.cruz@gmail.com>
2017-02-21 15:24:47 -0300

    wip - Handling /stocker/:ticker request

--- api_test.go
+++ api_test.go
@@ -41,3 +41,22 @@ func TestStocks(t *testing.T) {
 	assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
 	assert.Equal(t, "[{\"ticker\":\"PETR4\",\"name\":\"Petróleo Brasileiro\"},{\"ticker\":\"ELPL3\",\"name\":\"Eletropaulo\"}]", string(body))
 }
+
+//TODO: This test is dummy
+func TestStocksData(t *testing.T) {
+
+	handler := callStockData()
+
+	req, _ := http.NewRequest("GET", "/stocks/PETR4", nil)
+	w := httptest.NewRecorder()
+
+	handler.ServeHTTP(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	assert.Equal(t, http.StatusOk, resp.StatusCode)
+	assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
+	assert.Equal(t, "[{\"01/01/2017\":\"39.40}, {\"02/01/2017\": \"39.00\"}]")
+
+}
